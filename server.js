7912db96637c25b8c02b26ec09875d2af234992a
NemanjaP83 <necaintruder@gmail.com>
2022-06-17 13:37:05 +0200

    db connect

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const colors = require("colors");
+const { dbConnect } = require("./config/db.connect");
 
-const invoicesRoutes = require("./routes/invoices");
+const invoicesRoutes = require("./routes/invoices.routes");
 require("dotenv").config();
 
 const app = express();
+dbConnect();
+colors.enable();
 
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 // log middleware
 app.use((req, res, next) => {
   console.log(req.method, req.path);
@@ -16,11 +21,11 @@ app.use((req, res, next) => {
 });
 
 // invoices routes
-app.use("/api/invoices", invoicesRoutes);
+app.use(invoicesRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // listen for requests
 app.listen(`${PORT}`, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`.brightYellow);
 });
